51fd1bc77efff14e357b859a36e8e34f4412f20c
Gideo562 <codysmith562@gmail.com>
2019-01-24 10:46:48 -0800

    Commit Message

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,11 +8,34 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Too many questions is the Chan disease.");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
+        $("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
+    
+    $("a.thumbnail").click(projectClick);
+
+
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    
+    e.preventDefault();  
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else{
+        
+        $(".project-description").fadeOut();
+
+    }   
 }
